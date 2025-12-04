1a2f62d86447397a3e9f8bd343b5e5f3147711a3
aakdee01 <aakash.deep@protiviti.co.in>
2025-12-04 19:59:33 +0530

    09-create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,4 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn';
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, save more';
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An Amazon clone build with Nextjs and Mongodb';
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
